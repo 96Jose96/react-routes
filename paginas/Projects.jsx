d8b7d327f3bebd7b23e910d480dd7c8509fad13f
Jose <jm.diaz6620@gmail.com>
2024-10-26 14:08:43 +0200

    seguir con resume

--- paginas/Projects.jsx
+++ paginas/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import projects from "../data/projects"
 
 function Projects() {
     return (
@@ -9,6 +10,17 @@ function Projects() {
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/Resume'}>Resume</Link></li>
             </ul>
+           
+                {projects.map((project) => (
+                    <div>
+                        <h2>{project.name}</h2>
+                        <h3>ID Project {project.id}</h3>
+                        <img src={project.image}/>
+                        <p>{project.description}</p>
+                        <p>{project.url}</p>
+                    </div>
+                ))}
+            
         </>
     )
 }
